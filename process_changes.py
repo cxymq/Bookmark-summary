6c08bf93f062a65f57d7fa8925dedc2a5dc544d2
nazimai <809670160@qq.com>
2024-10-24 15:57:28 +0800

    Update process_changes.py

--- process_changes.py
+++ process_changes.py
@@ -167,7 +167,7 @@ def build_summary_file(title: str, url: str, summary: str, one_sentence: str) ->
 
 def build_summary_readme_md(summarized_bookmarks: List[SummarizedBookmark]) -> str:
     initial_prefix: str = """# Bookmark Summary 
-读取 bookmark-collection 中的书签，使用 jina reader 获取文本内容，然后使用 LLM 总结文本。详细实现请参见 process_changes.py。需要和 bookmark-collection 中的 Github Action 一起使用。
+读取 [Recently-read](https://github.com/cxymq/Recently-read) 中的书签，使用 jina reader 获取文本内容，然后使用 LLM 总结文本。详细实现请参见 process_changes.py。需要和 [Recently-read](https://github.com/cxymq/Recently-read) 中的 Github Action 一起使用。
     
 ## Summarized Bookmarks
 """
